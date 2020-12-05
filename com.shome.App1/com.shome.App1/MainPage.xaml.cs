adf5cc9b824811220bc1de5f711a210f75d342b0
Peter <sheraium@gmail.com>
2020-12-05 22:27:30 +0800

    Add project files.

---
+++ com.shome.App1/com.shome.App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace com.shome.App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Label1.Text = DateTime.Now.ToString("hh:mm:ss.fff");
+                await Task.Delay(500);
+            }
+        }
+    }
+}
